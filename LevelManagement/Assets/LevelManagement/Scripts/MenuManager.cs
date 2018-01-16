1c670b584af386a038f053f4aa0c7ba936d9d8c7
WilmerLin <unitypixelninja@gmail.com>
2018-01-16 10:37:33 -0800

    306 Menu Management Part 1
    created Menu and MenuManager class, setup menus as Prefabs

---
+++ LevelManagement/Assets/LevelManagement/Scripts/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public Menu mainMenuPrefab;
+
+        public Menu settingsMenuPrefab;
+
+        public Menu creditsScreenPrefab;
+
+        [SerializeField]
+        Transform menuParent;
+
+        private void Awake()
+        {
+            InitializeMenus();
+        }
+
+        private void InitializeMenus()
+        {
+            if (menuParent == null)
+            {
+                GameObject menuParentObject = new GameObject("Menus");
+                menuParent = menuParentObject.transform;
+            }
+
+            Menu[] menuPrefabs = { mainMenuPrefab, settingsMenuPrefab, creditsScreenPrefab };
+
+            foreach (Menu prefab in menuPrefabs)
+            {
+                if (prefab != null)
+                {
+                    Menu menuInstance = Instantiate(prefab, menuParent);
+
+                    if (prefab != mainMenuPrefab)
+                    {
+                        menuInstance.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        // open the main menu
+                    }
+                }
+            }
+        }
+
+    }
+}
