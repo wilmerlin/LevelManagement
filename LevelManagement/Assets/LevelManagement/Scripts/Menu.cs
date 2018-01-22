e430bd72f83ec14e95c9f532c944a3d9c0ef29bc
WilmerLin <unitypixelninja@gmail.com>
2018-01-22 12:37:12 -0800

    308 Menu Management Part 3A
    *Menu class: OnSettingsPressed, OnCreditsPressed, OnBackPressed
    *OnClick events hooked up to buttons in Prefabs

--- LevelManagement/Assets/LevelManagement/Scripts/Menu.cs
+++ LevelManagement/Assets/LevelManagement/Scripts/Menu.cs
@@ -20,17 +20,31 @@ namespace LevelManagement
 
         public void OnSettingsPressed()
         {
-
+            MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+            Menu settingsMenu = transform.parent.Find("SettingsMenu(Clone)").GetComponent<Menu>();
+            if (menuManager != null && settingsMenu != null)
+            {
+                menuManager.OpenMenu(settingsMenu);
+            }
         }
 
         public void OnCreditsPressed()
         {
-            
+            MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+            Menu creditsScreen = transform.parent.Find("CreditsScreen(Clone)").GetComponent<Menu>();
+            if (menuManager != null && creditsScreen != null)
+            {
+                menuManager.OpenMenu(creditsScreen);
+            }
         }
 
         public void OnBackPressed()
         {
-
+            MenuManager menuManager = Object.FindObjectOfType<MenuManager>();
+            if (menuManager != null)
+            {
+                menuManager.CloseMenu();
+            }
         }
     }
 }
